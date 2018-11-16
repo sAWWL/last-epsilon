3694247d5cf076e61a65e102fd2435f682f5b8d8
Austin Allen <otonomee@gmail.com>
2018-11-16 02:55:44 -0700

    Server window: remove username & port; validate IP address entry *we should have separate windows for add & edit*

--- Project Epsilon/AddMachine.xaml.cs
+++ Project Epsilon/AddMachine.xaml.cs
@@ -26,14 +26,10 @@ namespace Project_Epsilon
             {
                 string machinedata = Machines.MachineData[Machines.MachineIdx];
                 string machinename = machinedata.Split('-')[1];
-                string machineusername = machinedata.Split('-')[0].Split('@')[0];
                 string machineaddress = machinedata.Split('-')[0].Split('@')[1].Split(':')[0];
-                string machineport = machinedata.Split('-')[0].Split('@')[1].Split(':')[1];
 
                 machineName.Text = machinename;
                 machineAddress.Text = machineaddress;
-                machinePort.Text = machineport;
-                machineUsername.Text = machineusername;
             }
             
         }
@@ -41,12 +37,62 @@ namespace Project_Epsilon
         {
             if(Machines.MachineIdx == -1)
             {
-                Machines.MachineData.Add(machineUsername.Text + "@" + machineAddress.Text + ":" + machinePort.Text + "-" + machineName.Text);
+                Machines.MachineData.Add(machineName.Text + "@" + machineAddress.Text + ":21");
             }else
             {
-                Machines.MachineData[Machines.MachineIdx] = machineUsername.Text + "@" + machineAddress.Text + ":" + machinePort.Text + "-" + machineName.Text;
+                Machines.MachineData[Machines.MachineIdx] = machineName.Text + "@" + machineAddress.Text + ":21";
             }
             this.Close();
+        }
+
+        private void AddressEntry_LostFocus(object sender, RoutedEventArgs e)
+        {
+            try // incase there are no periods in string - otherwise would break on .split('.')
+            {
+                string[] test = machineAddress.Text.Split('.');
+                int isInteger;
+                if (int.TryParse(test[0], out isInteger)) // If numeric IP address instead of string
+                {
+
+                    IPAddressError.Visibility = Visibility.Hidden;
+                    int[] octets = new int[4];
+                    int counter = 0;
+                    bool isCorrectAddress = true;
+                    foreach (string octet in machineAddress.Text.Split('.'))
+                    {
+                        octets[counter] = Convert.ToInt32(octet);
+                        if (!Enumerable.Range(0, 256).Contains(Convert.ToInt32(octet)))
+                        {
+                            isCorrectAddress = false;
+                            break;
+                        }
+                        else
+                        {
+                            isCorrectAddress = true;
+                        }
+                        counter += 1;
+                    }
+                    if (!isCorrectAddress)
+                    {
+                        IPAddressError.Visibility = Visibility.Visible;
+                    }
+
+                    if (octets[0] == 192 || octets[0] == 127)
+                    {
+                        ;
+                    }
+                    else
+                    {
+                        IPAddressError.Visibility = Visibility.Visible;
+                        IPAddressError.Text = "Invalid IP Address - Must Be '192.x.x.x' Or '127.0.0.1'";
+                    }
+                }
+            } catch {; }
+        }
+
+        private void AddressEntry_OnFocus(object sender, RoutedEventArgs e)
+        {
+            IPAddressError.Visibility = Visibility.Hidden;
         }
 
         private void CancelAddBtn_Click(object sender, RoutedEventArgs e)
